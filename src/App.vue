42de3db1e28751e3447f4c7f752d1c3754767bd3
ZhangSir <57878778@qq.com>
2023-08-02 11:36:03 +0800

    Basic content layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,86 @@
-<script setup>
-</script>
-
 <template>
-  <el-row class="mb-4">
-    <el-button round>Round</el-button>
-    <el-button type="primary" round>Primary</el-button>
-    <el-button type="success" round>Success</el-button>
-    <el-button type="info" round>Info</el-button>
-    <el-button type="warning" round>Warning</el-button>
-    <el-button type="danger" round>Danger</el-button>
-  </el-row>
+  <div class="table_box">
+    <div class="title">
+      <h2>最简单的CRUD DEMO</h2>
+    </div>
+    <div class="main">
+      <el-input v-model="input" placeholder="请输入姓名搜索🔍" />
+      <el-button type="primary">添加</el-button>
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column fixed prop="date" label="Date" width="150" />
+        <el-table-column prop="name" label="Name" width="120" />
+        <el-table-column prop="state" label="State" width="120" />
+        <el-table-column prop="city" label="City" width="120" />
+        <el-table-column prop="address" label="Address" width="600" />
+        <el-table-column prop="zip" label="Zip" width="120" />
+        <el-table-column fixed="right" label="Operations" width="120">
+          <template #default>
+            <el-button link type="primary" size="small" @click="handleRawClick">Detail</el-button>
+            <el-button link type="primary" size="small">Edit</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import { ref } from 'vue';
 
+/** 处理data信息 */ 
+const input = ref("")
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },
+]
+
+/** 处理事件函数 */
+const handleRawClick = () => {
+  console.log('handleRawClick')
+} 
+
+</script>
+
+<style scoped>
+.table_box {
+  width: 800px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
